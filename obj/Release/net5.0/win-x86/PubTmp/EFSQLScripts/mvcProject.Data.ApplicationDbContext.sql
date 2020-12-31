eb000074df3d583b5c2935332a9becf48f6a5f77
Ryan Hutson <ryanhutson5518@gmail.com>
2020-12-31 13:18:18 -0600

    Testing

---
+++ obj/Release/net5.0/win-x86/PubTmp/EFSQLScripts/mvcProject.Data.ApplicationDbContext.sql
@@ -0,0 +1,57 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20201230171411_addCategoryToDatabase')
+BEGIN
+    CREATE TABLE [Category] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        [DisplayOrder] int NOT NULL,
+        CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20201230171411_addCategoryToDatabase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20201230171411_addCategoryToDatabase', N'5.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20201230195804_AddApplicationTypeToDatabase')
+BEGIN
+    CREATE TABLE [ApplicationType] (
+        [Id] int NOT NULL IDENTITY,
+        [Name] nvarchar(max) NULL,
+        CONSTRAINT [PK_ApplicationType] PRIMARY KEY ([Id])
+    );
+END;
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20201230195804_AddApplicationTypeToDatabase')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20201230195804_AddApplicationTypeToDatabase', N'5.0.1');
+END;
+GO
+
+COMMIT;
+GO
+
